98f395df3dd3fb8263e362b70ce39b8133b4b107
Ciro Pizza <129529624+cyropi@users.noreply.github.com>
2023-04-15 19:55:48 +0200

    Update Viste.sql

--- Database/GalleriaFotograficaSQL/Viste.sql
+++ Database/GalleriaFotograficaSQL/Viste.sql
@@ -1,1 +1,6 @@
-
+CREATE OR REPLACE VIEW public.numero_totale_fotografie_e_utenti
+ AS
+ SELECT ( SELECT count(*) AS count
+           FROM utente) AS totale_utenti,
+    ( SELECT count(*) AS count
+           FROM fotografia) AS totale_foto;
