32e9c0d5c67ef357e7d4bcdb77db69b7192f33cb
Ciro Pizza <129529624+cyropi@users.noreply.github.com>
2023-04-16 02:22:29 +0200

    Update Popolamento.sql

--- Database/GalleriaFotograficaSQL/Popolamento.sql
+++ Database/GalleriaFotograficaSQL/Popolamento.sql
@@ -1,6 +1,7 @@
 
--- sds
-CALL crea_utente( 'mario', 'rossi', 'mariorossi98@gmail.com', 'italia', 'rossimario12345' )
-CALL crea_utente( 'francesco', 'conte', 'francescoconte95@outlook.com', italia', 'contefrancesco32875' )
-CALL crea_utente( 'sofia', 'ferrari', 'sofiaferrari87@libero.it', 'italia', 'ferrarisofia53765' )
-CALL crea_utente( 'pasquale', 'esposito', 'pasqualeesposito92@virgilio.it', 'italia', 'espositopasquale45112' )
+-- CREAZIONE UTENTI
+
+CALL crea_utente( 'mario', 'rossi', 'mariorossi98@gmail.com', 'italia', 'rossimario12345' );
+CALL crea_utente( 'francesco', 'conte', 'francescoconte95@outlook.com', italia', 'contefrancesco32875' );
+CALL crea_utente( 'sofia', 'ferrari', 'sofiaferrari87@libero.it', 'italia', 'ferrarisofia53765' );
+CALL crea_utente( 'pasquale', 'esposito', 'pasqualeesposito92@virgilio.it', 'italia', 'espositopasquale45112' );
